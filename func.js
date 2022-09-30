c21f09046c66389ede50d962c3bed1b26473b224
ganaviramesh <68916136+ganaviramesh@users.noreply.github.com>
2022-09-30 22:02:29 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,17 @@
+function sum(n) {
+    var r=0;
+    while (n!=0){
+        r=r+(n%10);
+        n=Math.floor(n/10);
+    }
+    return r;
+}
+function myFunction() {
+  var n = document.getElementById("num").value;
+  var r= sum(n);
+  if (r>=10){
+     r=sum(r);}
+  alert("Lucky number is "+r);
+
+ 
+}
